c0837a12e3c3c8c87315928e6156d9866bc96ce4
ainanax <catyjamei@gmail.com>
2026-02-22 10:07:05 +0800

    exam

--- Advanced Python Concepts/Advanced_Practice_Set/01_decoratorhello.py
+++ Advanced Python Concepts/Advanced_Practice_Set/01_decoratorhello.py
@@ -1,7 +1,12 @@
+def logger(func):
+    def wrapper():
+        print("Function is being called!")
+        func()
+    return wrapper
 
 @logger
 def say_hello():
     print("Hello!")
 
 
-@logger
+say_hello()
