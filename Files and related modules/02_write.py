fcb30f0036c78c9ee2a70c1d00f8353fce0c0b53
ainanax <catyjamei@gmail.com>
2026-03-11 16:54:12 +0800

    write read append

---
+++ Files and related modules/02_write.py
@@ -0,0 +1,7 @@
+f = open("jamie.txt","w")
+
+string = '''
+Jamie need to push more
+
+'''
+f.write(string)
---
+++ Files and related modules/03_append.py
@@ -0,0 +1,11 @@
+# Append to an existing file called John Doe.txt 
+# It should add data about John Doe's Hometown
+
+f = open("jamie.txt", "a")
+
+string = '''
+John Doe initially lived in Phoenix, Arizona. He is a very nice guy
+'''
+f.write(string)
+
+f.close()
