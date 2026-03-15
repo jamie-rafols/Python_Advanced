a810d914a00c1997d74b1f032b66d306911e2acb
ainanax <catyjamei@gmail.com>
2026-03-16 07:46:11 +0800

    save

--- Files and related modules/Practice set Files and related modules/08_countlines2.py
+++ Files and related modules/Practice set Files and related modules/08_countlines2.py
@@ -1,10 +1,10 @@
-import sys
+import sys 
 
 def count_lines(filename):
     with open(filename) as f:
         return len(f.readlines())
     
-    
+
 if __name__ == "__main__":
     filename = sys.argv[1]
     num_lines = count_lines(filename)
