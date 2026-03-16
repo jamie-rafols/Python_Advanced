e13d6b887ed2346200ef21f32cf142132a1c8634
ainanax <catyjamei@gmail.com>
2026-03-16 11:48:04 +0800

    virtual env

--- External Modules/01_venv.py
+++ External Modules/01_venv.py
@@ -1,6 +1,13 @@
 
 # pip install virualenv
+#pip list (list all module installed )
+#pip install --upgrade {name of module you want to upgrade}
+#pip uninstall {name of the module you want to remove}
+#pip freeze (shows the package name, version number for all the packages)
+# pip freeze > requirement.txt
+#other users that want to use your project can simply type in command "pip install requirements.txt"
 # python -m venv env1
+# .env1\Script\Activate.ps1
 # v1 - package 1 (pandas)
 # v2 - package 2 (numpy)
 # v3 - package 3 (moviepy)
